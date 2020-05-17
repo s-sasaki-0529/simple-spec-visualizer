7eb85fba0024125011d8dc4f1054530235681a85
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-05-18 01:36:47 +0900

    タブのスタイルを微修正

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import GeneralPage from "./Generate";
 import DetailPage from "./Detail";
+import { makeStyles } from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
 
-function App() {
+const useStyles = makeStyles({
+  tabs: {
+    borderBottom: "1px solid",
+    borderColor: grey[400],
+  },
+  container: {
+    margin: 15,
+  },
+});
+
+export default () => {
   const [tabValue, setTabValue] = React.useState(1);
 
   return (
     <div>
       <Tabs
+        className={useStyles().tabs}
         value={tabValue}
         indicatorColor="primary"
         textColor="primary"
@@ -20,9 +34,9 @@ function App() {
         <Tab label="Details" />
       </Tabs>
 
-      {tabValue === 0 ? GeneralPage() : DetailPage()}
+      <Container className={useStyles().container}>
+        {tabValue === 0 ? GeneralPage() : DetailPage()}
+      </Container>
     </div>
   );
-}
-
-export default App;
+};
