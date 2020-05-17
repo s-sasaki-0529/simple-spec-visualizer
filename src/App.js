1fb49396183cc8a00bc433a321278840ae61fca6
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-05-17 22:19:58 +0900

    material-uiを導入してタブを試しに入れてみる

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 function App() {
-  return <div>Hello, World!!</div>;
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const GeneralPage = () => <div>General</div>;
+
+  const DetailPage = () => <div>Details</div>;
+
+  return (
+    <div>
+      <Tabs
+        value={tabValue}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={(_, newValue) => setTabValue(newValue)}
+      >
+        <Tab label="General" />
+        <Tab label="Details" />
+      </Tabs>
+
+      {tabValue === 0 ? GeneralPage() : DetailPage()}
+    </div>
+  );
 }
 
 export default App;
