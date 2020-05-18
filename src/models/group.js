c4717d08541b2f48ea35c7ca652f922b4723edfe
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-05-19 00:44:21 +0900

    フォーマットにあわせてモデル修正

--- src/models/group.js
+++ src/models/group.js
@@ -7,10 +7,12 @@ export default class Group {
     this.examples = [];
 
     Object.keys(children).forEach((childName) => {
-      if (childName === "examples") {
-        this.examples = Object.keys(children["examples"]).map((exampleName) => {
-          return new Example(exampleName, children["examples"][exampleName]);
-        });
+      if (childName === "_examples") {
+        this.examples = Object.keys(children["_examples"]).map(
+          (exampleName) => {
+            return new Example(exampleName, children["_examples"][exampleName]);
+          }
+        );
       } else {
         this.children.push(new Group(childName, children[childName]));
       }
