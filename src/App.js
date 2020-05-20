6383a45a7120d9a2cfaa11d92b1e337a6762219b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-05-21 04:57:33 +0900

    レイアウトのしやすさって観点でタブをサイドメニューに変更

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
 import TabGeneral from './components/TabGenerate'
 import TabDetail from './components/TabDetail'
-import { grey } from '@material-ui/core/colors'
 import Report from './models/report'
 import dummy from './dummy.json'
+import Drawer from '@material-ui/core/Drawer'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import List from '@material-ui/core/List'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,7 +19,7 @@ export default class App extends React.Component {
   }
 
   tabContent() {
-    if (this.state.tabValue === 0) {
+    if (this.state.tabValue === 'General') {
       return <TabGeneral report={this.state.report} />
     } else {
       return <TabDetail report={this.state.report} />
@@ -28,34 +28,25 @@ export default class App extends React.Component {
 
   render() {
     const styles = {
-      tabs: {
-        position: 'sticky',
-        top: 0,
-        height: 40,
-        borderBottom: '1px solid #0006',
-        backgroundColor: 'white',
-        zIndex: 100
-      },
       tabContentWrapper: {
-        marginTop: 50,
+        marginTop: 10,
+        marginLeft: 60,
         minWidth: 1280 + 15
       }
     }
     return (
       <div>
-        <Tabs
-          value={this.state.tabValue}
-          indicatorColor="primary"
-          textColor="primary"
-          onChange={(_, newValue) => this.setTabValue(newValue)}
-          centered
-          style={styles.tabs}
-        >
-          <Tab label="General" />
-          <Tab label="Details" />
-        </Tabs>
+        <Drawer variant="permanent" anchor="left">
+          <List>
+            {['General', 'Detail'].map(text => (
+              <ListItem button key={text} onClick={() => this.setTabValue(text)}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
 
-        <Container style={styles.tabContentWrapper}>{this.tabContent()}</Container>
+        <div style={styles.tabContentWrapper}>{this.tabContent()}</div>
       </div>
     )
   }
