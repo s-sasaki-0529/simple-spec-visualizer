f4c8823126d290894cf172cab94236f3829285f4
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-06-05 01:12:53 +0900

    ダッシュボード上のFailedExampleにカーソルを合わせると原因を表示するように

--- src/components/dashboard/DashboardFailedExamples.tsx
+++ src/components/dashboard/DashboardFailedExamples.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List, ListItemText, Divider } from '@material-ui/core'
+import { Tooltip, List, ListItemText, Divider } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import { red } from '@material-ui/core/colors'
 import Example from '../../models/example'
@@ -8,18 +8,24 @@ import Example from '../../models/example'
  * 失敗したExampleリストを描画するコンポーネント
  */
 type Props = {
-  height: number,
+  height: number
   examples: Example[]
 }
-const DashboardFailedExamples:React.FunctionComponent<Props> = (props) => {
+const DashboardFailedExamples: React.FunctionComponent<Props> = props => {
   const StyledList = withStyles({
     root: {
       maxHeight: props.height,
       overflow: 'scroll',
       whiteSpace: 'nowrap',
-      color: red[500]
+      color: red[500],
+      cursol: 'pointer'
     }
   })(List)
+  const StyledTooltip = withStyles({
+    tooltip: {
+      fontSize: '1em'
+    }
+  })(Tooltip)
 
   if (props.examples.length === 0) {
     return <span>There are no failed examples.</span>
@@ -29,7 +35,9 @@ const DashboardFailedExamples:React.FunctionComponent<Props> = (props) => {
     <StyledList aria-label="secondary mailbox folders">
       {props.examples.map(example => (
         <div key={example.id}>
-          <ListItemText primary={example.getFullText()} />
+          <StyledTooltip title={example.exception} placement="top">
+            <ListItemText primary={example.getFullText()} />
+          </StyledTooltip>
           <Divider />
         </div>
       ))}
