f976064282c3c43f7f5c4bdb136f2ec1c2e3137e
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-05-30 17:29:34 +0900

    VFCで書き直せるコンポーネントを書き直し

--- src/components/UIResults/UIResultsLeftPainSearchInput.tsx
+++ src/components/UIResults/UIResultsLeftPainSearchInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { VFC } from 'react'
 import { TextField, InputAdornment } from '@material-ui/core'
 import { Search as SearchIcon, Clear as ClearIcon } from '@material-ui/icons'
 
@@ -7,27 +7,27 @@ type Props = {
   onSubmit: (v: string) => void
 }
 
-export default class SearchInput extends React.Component<Props, {}> {
-  render() {
-    return (
-      <TextField
-        value={this.props.value}
-        onChange={event => this.props.onSubmit(event.target.value)}
-        placeholder="Search"
-        style={{ width: '100%' }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-          endAdornment: (
-            <InputAdornment position="end">
-              {this.props.value && <ClearIcon style={{ cursor: 'pointer' }} onClick={() => this.props.onSubmit('')} />}
-            </InputAdornment>
-          )
-        }}
-      />
-    )
-  }
+const SearchInput: VFC<Props> = props => {
+  return (
+    <TextField
+      value={props.value}
+      onChange={event => props.onSubmit(event.target.value)}
+      placeholder="Search"
+      style={{ width: '100%' }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment position="end">
+            {props.value && <ClearIcon style={{ cursor: 'pointer' }} onClick={() => props.onSubmit('')} />}
+          </InputAdornment>
+        )
+      }}
+    />
+  )
 }
+
+export default SearchInput
